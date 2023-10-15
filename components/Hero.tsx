d3d484bcd2bde626cdc26a52eb79ddce4337590a
Omolara5861 <debbiegterra@gmail.com>
2023-10-15 15:16:10 +0100

    refactor: :recycle: Change color

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <div className="bg-hero-image bg-cover bg-center h-[200px] rounded-3xl mb-20">
       <div className="bg-primaryColor bg-opacity-30 h-[200px] rounded-3xl">
       </div>
-        <form className=" flex gap-4 bg-white rounded-[20px] w-[88%] mx-auto min-h-[60px] p-4 -mt-6">
+        <form className=" flex gap-4 bg-white rounded-[20px] w-[88%] mx-auto min-h-[60px] p-4 -mt-6 font-bold">
           <div className="flex-1 flex gap-2">
 
           <div className="flex items-center gap-3 border-r-[1px] mr-4 pr-4">
@@ -14,7 +14,7 @@ const HeroSection = () => {
 
 <PiMapPin />
             </div>
-            <input type="text"  className="outline-none w-[160px]" placeholder='Where are you going?' />
+            <input type="text"  className="outline-none w-[170px]" placeholder='Where Are You Going?' />
       <div className="ml-3">
 
             <RxChevronDown/>
@@ -33,11 +33,11 @@ const HeroSection = () => {
 </div>
             </div>
             <div className="flex items-center gap-3 ">
-            <div className="text-gray-400 text-[20px]">
+            <div className="text-gray-900 text-[20px]">
 
 <PiUser />
             </div>
-            <input type="text" className="outline-none w-[150px]" placeholder='3 Adults' />
+            <input type="text" className="outline-none w-[150px] placeholder-gray-900" placeholder='3 adults' />
 
           <div className="ml-3">
 
