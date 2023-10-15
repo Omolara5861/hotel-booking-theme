c1c8cb330c3562d67b22c1d9927b9f85fb13828e
Omolara5861 <debbiegterra@gmail.com>
2023-10-15 15:02:38 +0100

    refactor: :recycle: Add other component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter,Quicksand } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
+import DestinationSection from '@/components/DestinationSection'
+import HotelsSection from '@/components/HotelsSection'
+import ExploreFranceSection from '@/components/ExploreFranceSection'
 
-const inter = Inter({ subsets: ['latin'] })
+const quicksand = Quicksand({ subsets: ['latin'],weight:['400','500','600','700'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex ${inter.className}`}
+      className={`flex min-h-100vh mx-auto max-w-[1400px] ${quicksand.className}`}
     >
      <Sidebar />
-     <section className="flex-1 h-screen px-7">
+     <section className=" w-full flex-1 px-6 pb-6 ">
      <Navbar />
      <Hero/>
+     <DestinationSection/>
+     <HotelsSection/>
+     <ExploreFranceSection />
      </section>
     </main>
   )
